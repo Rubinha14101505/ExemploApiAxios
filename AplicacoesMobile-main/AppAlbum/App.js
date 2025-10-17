1695ee9381fb1976ed7a7a080c8793d9bad32ed9
Rubinha14101505 <anarubiaalmeida1410@gmail.com>
2025-10-17 19:46:35 -0300

    Entrega das atividades

--- AplicacoesMobile-main/AppAlbum/App.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import Album from "./components/Album"
-export default function App(){
-  return <Album/>
-}
--- AplicacoesMobile-main/PrevisaoDoTempo/src/service/api.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-// cria objeto API para conexão 
-const api = axios.create({
-    timeout:2000
-})
-export default api;
-
